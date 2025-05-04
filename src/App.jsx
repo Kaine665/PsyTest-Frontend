405dc591df50c44616bce02538fcb2291812a82c
Kaine <155346835+1367-5@users.noreply.github.com>
2025-05-04 23:13:40 +0800

    尝试使用hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, HashRouter, Route } from "react-router-dom";
 import Chat from "./components/Chat";
 import Chats from "./components/Chats";
 import Login from "./components/Login";
@@ -6,7 +6,7 @@ import NewChat from "./components/NewChat";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Chats />} />
         <Route path="login" element={<Login />} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="chats" element={<Chats />} />
         <Route path="newchat" element={<NewChat />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
